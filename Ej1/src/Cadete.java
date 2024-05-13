3fdf0c6213121f30faaafa493c7dc32531cf1c78
ghami20 <ghamiboxbox@gmail.com>
2024-05-13 10:25:38 -0300

    Primer commit

---
+++ Ej1/src/Cadete.java
@@ -0,0 +1,13 @@
+
+public class Cadete extends Empleado{
+
+	private String zona;
+
+	public Cadete(String nombre, double sueldo, String zona) {
+		super(nombre, sueldo);
+		this.zona = zona;
+	}
+	
+	
+	
+}
---
+++ Ej1/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Cadete nuevo = new Cadete("Gamaliel",199,"Ventas");
+		nuevo.getNombre();
+		
+	}
+
+}
